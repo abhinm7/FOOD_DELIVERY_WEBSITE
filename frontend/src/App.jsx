7a8fc005784844e5ad1401aa3d0618542b8a8341
ABHIN M <abhin18m@gmail.com>
2024-07-13 00:04:15 +0530

    Directories and paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Placeholder from './pages/placeholder/Placeholder'
+import Cart from './pages/Cart/Cart'
 
-
-const App = () => {
+function App() {
   return (
-    <div>
-      <Navbar/>
+    <div className='app'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/placeholder' element={<Placeholder />} />
+      </Routes>
     </div>
   )
 }
