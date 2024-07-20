87d911a70624c0ac5a8a1028e1f46c7b19553813
ABHIN M <abhin18m@gmail.com>
2024-07-21 00:26:34 +0530

    cart completed

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './AppDownload.css'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
         <p>For better experience <br /> Download Tomato App</p>
         <div className="app-download-platforms">
-            <img src={assets.play_store} alt="" />
-            <img src={assets.app_store} alt="" />
+            <Link to='https://play.google.com/store/games?hl=en'><img src={assets.play_store} alt="" /></Link>
+            <Link to='https://www.apple.com/in/app-store/'><img src={assets.app_store} alt="" /></Link>
         </div>
     </div>
   )
